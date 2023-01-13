10e4f0398ea9b7e2440efee721321dabfabaeb20
Roshan Hegde <roshanhegde@outlook.com>
2023-01-12 20:15:22 -0500

    started player class in game.py

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple pong game.
---
+++ game.py
@@ -0,0 +1,11 @@
+import turtle
+from tkinter import Tk
+
+turtle.screensize(canvwidth=600, canvheight=600)
+turtle.getcanvas().attributes('-type', 'dialog')
+turtle.screensize(canvwidth=600, canvheight=600)
+a = turtle.Turtle()
+class Player:
+    def __init__(starting_pos):
+        self.box = turtle.Turtle().turtlesize
+
